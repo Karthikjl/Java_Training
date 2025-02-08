bf8254cb13bcfcb74620c8ab87907667ce4cdb2a
KARTHIKEYAN <kar333thic@gmail.com>
2025-02-08 18:47:25 +0530

    Commited Assignment

---
+++ Assignment2/Program10.java
@@ -0,0 +1,46 @@
+package com.celcom.Assignment2;
+
+import java.util.Scanner;
+
+class Shape {
+	void getArea() {
+		System.out.println("Not declared");
+	}
+}
+
+class Rectangles extends Shape {
+	float width;
+	float height;
+	float area;
+
+	Rectangles(float width, float height) {
+		this.height = height;
+		this.width = width;
+	}
+
+	void getArea() {
+		area = height * width;
+		System.out.printf("The area of Rectangle: %.2f\n", area);
+	}
+}
+
+public class Program10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner inputScanner = new Scanner(System.in);
+		float width;
+		float height;
+		System.out.println("Rectangle area Program\n");
+		System.out.print("Enter the width & height: ");
+		width = inputScanner.nextFloat();
+		height = inputScanner.nextFloat();
+		
+		Shape recShape = new Rectangles(width, height);
+		recShape.getArea();
+		
+		System.out.println("\nThankYou");
+
+	}
+
+}
